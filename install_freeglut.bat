ca1d5110a8fdcb9cf278e0dd0f489e285d4163fd
Ndik <98265563+IRedDragonICY@users.noreply.github.com>
2024-06-30 22:51:26 +0700

    add error handle bat installer

--- install_freeglut.bat
+++ install_freeglut.bat
@@ -1,28 +1,65 @@
 @echo off
+
 echo  ===================================================================
-echo  ^| ^< FreeGLUT Installation Script for Embarcadero Dev-C++ ^>    ^|
+echo  | < FreeGLUT Installation Script for Embarcadero Dev-C++ >    |
 echo  ===================================================================
 echo.
+
 echo  This script will install FreeGLUT for use with Embarcadero Dev-C++.
 echo.
 echo  Make sure you have extracted the FreeGLUT archive before running this script.
 echo.
 
-:check_default_path
+:get_install_path
+set "default_path=C:\Program Files (x86)\Embarcadero\Dev-Cpp"
+set "manual_path="
+
 echo  [1/4] Checking Default Installation Path...
 echo.
-set "default_path=C:\Program Files (x86)\Embarcadero\Dev-Cpp"
+echo  Default installation path: "%default_path%"
+echo.
+choice /C YN /M "Use default installation path? (Y/N) "
+if %errorlevel%==2 (
+    set /p manual_path="Enter the desired installation path: "
+    if not exist "%manual_path%" (
+        echo Error: Invalid installation path.
+        goto error
+    )
+) else (
+    set "manual_path=%default_path%"
+)
 
 :copy_files
 echo  [2/4] Copying Files...
 echo.
-xcopy /E /Y "%cd%\include\GL" "%manual_path%\TDM-GCC-64\x86_64-w64-mingw32\include\GL\"
+
+if not exist "%manual_path%\TDM-GCC-64\x86_64-w64-mingw32\include\GL" mkdir "%manual_path%\TDM-GCC-64\x86_64-w64-mingw32\include\GL"
+xcopy /E /Y "%cd%\include\GL" "%manual_path%\TDM-GCC-64\x86_64-w64-mingw32\include\GL\" >nul 2>&1
+if %errorlevel% neq 0 (
+    echo Error: Failed to copy include files. Please run the script as administrator.
+    goto error
+)
 echo  Copied include files...
-xcopy /Y "%cd%\lib\x64\*.a" "%manual_path%\TDM-GCC-64\x86_64-w64-mingw32\lib\"
+
+xcopy /Y "%cd%\lib\x64\*.a" "%manual_path%\TDM-GCC-64\x86_64-w64-mingw32\lib\" >nul 2>&1
+if %errorlevel% neq 0 (
+    echo Error: Failed to copy x64 lib files. Please run the script as administrator.
+    goto error
+)
 echo  Copied x64 lib files...
-xcopy /Y "%cd%\lib\*.a" "%manual_path%\TDM-GCC-64\x86_64-w64-mingw32\lib32\"
+
+xcopy /Y "%cd%\lib\*.a" "%manual_path%\TDM-GCC-64\x86_64-w64-mingw32\lib32\" >nul 2>&1
+if %errorlevel% neq 0 (
+    echo Error: Failed to copy x32 lib files. Please run the script as administrator.
+    goto error
+)
 echo  Copied x32 lib files...
-xcopy /Y "%cd%\bin\x64\freeglut.dll" "%systemroot%\System32\"
+
+copy /Y "%cd%\bin\x64\freeglut.dll" "%systemroot%\System32\" >nul 2>&1
+if %errorlevel% neq 0 (
+    echo Error: Failed to copy x64 DLL file. Please run the script as administrator.
+    goto error
+)
 echo  Copied x64 DLL file...
 echo.
 
@@ -30,14 +67,14 @@ echo.
 echo  [3/4] Installation Complete!
 echo.
 echo  ===================================================================
-echo  ^| ^< FreeGLUT has been successfully installed. ^>               ^|
+echo  | < FreeGLUT has been successfully installed. >               |
 echo  ===================================================================
 echo.
 echo  To use FreeGLUT in your projects, make sure to:
 echo.
-echo    1. Add #include ^<GL/freeglut.h^> to your source code.
+echo    1. Add #include <GL/freeglut.h> to your source code.
 echo    2. Link with the FreeGLUT library:
-echo       - Project Options ^> Parameters ^> Linker: -lfreeglut
+echo       - Project Options > Parameters > Linker: -lfreeglut
 echo    installer by IRedDragonICY
 echo    from Ahmad Dahlan University
 echo.
